9437f4099cf821f3f23be5be5e6089b7fa1a6ef0
Huymess11 <huymess0610@gmail.com>
2025-02-08 11:20:29 +0700

    commit Class, Score Controller

---
+++ Controllers/ClassController.cs
@@ -0,0 +1,54 @@
+﻿using API_Technology_Students_Manages.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Technology_Students_Manages.Controllers
+{
+    [RoutePrefix("api/Class")]
+    public class ClassController : ApiController
+    {
+        DBConnect DBConnect = new DBConnect();
+        [HttpGet]
+        [Route("layLopTheoID")]
+        public object LayLop(string maLop = null)
+        {
+            object lop = new List<object>();
+            DataTable dt = new DataTable();
+            SqlParameter[] selectparams = {
+                    new SqlParameter("@maLop", maLop),
+            };
+            dt = DBConnect.ExecuteQuery("SP_GET_CLASS_ID", selectparams);
+
+            if (dt?.Rows?.Count > 0)
+            {
+                lop = dt;
+                return lop;
+            }
+            return lop;
+        }
+        [HttpGet]
+        [Route("layDiemLop")]
+        public object LayDiemLop(string maLop = null)
+        {
+            object lop = new List<object>();
+            DataTable dt = new DataTable();
+            SqlParameter[] selectparams = {
+                    new SqlParameter("@maLop", maLop),
+            };
+            dt = DBConnect.ExecuteQuery("SP_GET_CLASS_SCORE_ID", selectparams);
+
+            if (dt?.Rows?.Count > 0)
+            {
+                lop = dt;
+                return lop;
+            }
+            return lop;
+        }
+    }
+}
---
+++ Controllers/ScoreController.cs
@@ -0,0 +1,36 @@
+﻿using API_Technology_Students_Manages.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Technology_Students_Manages.Controllers
+{
+    [RoutePrefix("api/Score")]
+    public class ScoreController : ApiController
+    {
+        DBConnect DBConnect = new DBConnect();
+        [HttpGet]
+        [Route("thongTinDiem")]
+        public object LayDiem(string maHV = null)
+        {
+            object diem = new List<object>();
+            DataTable dt = new DataTable();
+            SqlParameter[] selectparams = {
+                    new SqlParameter("@maHv", maHV),
+            };
+            dt = DBConnect.ExecuteQuery("SP_SHOW_INFOR_OF_STUDENTS", selectparams);
+
+            if (dt?.Rows?.Count > 0)
+            {
+                diem = dt;
+                return diem;
+            }
+            return diem;
+        }
+    }
+}
