30612524e781abd2ef26d0137479e7927db35855
Huymess11 <huymess0610@gmail.com>
2025-07-05 22:42:28 +0700

    commit SubjectController

---
+++ Models/MonHoc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API_Technology_Students_Manages.Models
+{
+    public class MonHoc
+    {
+        [Key] 
+        public string SubjectID { get; set; }     
+        public string SubjectName { get; set; }   
+        public string SemesterID { get; set; }      
+        public decimal TuitionFee { get; set; }     
+        public bool? DeleteFlag { get; set; }
+    }
+}
