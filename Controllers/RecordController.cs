3b41f17ada553cbbfbe9fdd3d9517a62d8abbfbf
Huymess11 <huymess0610@gmail.com>
2025-07-06 01:16:04 +0700

    commit update RecordController

---
+++ Controllers/RecordController.cs
@@ -0,0 +1,86 @@
+﻿using API_Students_Manager.Models;
+using API_Technology_Students_Manages.DataAccess;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API_Technology_Students_Manages.Models;
+
+namespace API_Technology_Students_Manages.Controllers
+{
+    [RoutePrefix("api/Record")]
+    public class RecordController : ApiController
+    {
+        DBConnect DBConnect = new DBConnect();
+
+        [HttpGet]
+        [Route("danhSachDiemDanh")]
+        public object DanhSachDiemDanh(string semesterID = null, string subjectID = null, string classID = null)
+        {
+            object record = new List<object>();
+            DataTable dt = new DataTable();
+            SqlParameter[] selectParams = {
+                new SqlParameter("@SemesterID", semesterID),
+                new SqlParameter("@SubjectID", subjectID),
+                new SqlParameter("@ClassID", classID)
+            };
+
+            dt = DBConnect.ExecuteQuery("SP_SELECT_ATTENDANCE_RECORD", selectParams);
+
+            if (dt?.Rows?.Count > 0)
+            {
+                record = dt;
+                return record;
+            }
+            return record;
+        }
+
+        [HttpPost]
+        [Route("themThongTinDiemDanh")]
+        public bool ThemThongTinDiemDanh([FromBody] DiemDanh data)
+        {
+            bool result = false;
+            string json = JsonConvert.SerializeObject(data);
+
+            SqlParameter[] insertParam = {
+                new SqlParameter("@json", json)
+            };
+
+            result = DBConnect.ExecuteNonQuery("SP_INSERT_ATTENDANCE_RECORD", insertParam);
+            return result;
+        }
+
+        [HttpPost]
+        [Route("suaThongTinDiemDanh")]
+        public bool DoiThongTinDiemDanh([FromBody] DiemDanh data)
+        {
+            bool result = false;
+            string json = JsonConvert.SerializeObject(data);
+
+            SqlParameter[] updateParam = {
+                new SqlParameter("@json", json)
+            };
+
+            result = DBConnect.ExecuteNonQuery("SP_UPDATE_ATTENDANCE_RECORD", updateParam);
+            return result;
+        }
+        [HttpPost]
+        [Route("xoaThongTinDiemDanh")]
+        public bool XoaThongTinDiemDanh(string attendanceID)
+        {
+            bool result = false;
+
+            SqlParameter[] deleteParams = {
+                new SqlParameter("@AttendanceID", attendanceID)
+            };
+
+            result = DBConnect.ExecuteNonQuery("SP_DELETE_ATTENDANCE_RECORD", deleteParams);
+            return result;
+        }
+    }
+}
