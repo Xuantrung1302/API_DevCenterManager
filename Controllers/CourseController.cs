b2062c0f67582075c7771ac680dd8feec611dd3b
Huymess11 <huymess0610@gmail.com>
2025-07-14 23:57:22 +0700

    commit update Course

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -15,64 +15,87 @@ namespace API_Technology_Students_Manages.Controllers
     public class CourseController : ApiController
     {
         DBConnect DBConnect = new DBConnect();
-
-        //API Course
         [HttpGet]
-        [Route("thongTinKhoaHoc")]
-        public object DanhSachKhoaHoc()
+        [Route("chiTietChuongTrinhHoc")]
+        public object DanhSachChiTietKhoaHoc(string courseId = null, string semesterName = null, string subjectName = null)
         {
-            object khoaHoc = new List<object>();
+            object courseDetail = new List<object>();
             DataTable dt = new DataTable();
 
-            dt = DBConnect.ExecuteQuery("SP_SELECT_SEARCH_COURSE");
+            SqlParameter[] selectparams = {
+        new SqlParameter("@CourseID", (object)courseId ?? DBNull.Value),
+        new SqlParameter("@SemesterName", (object)semesterName ?? DBNull.Value),
+        new SqlParameter("@SubjectName", (object)subjectName ?? DBNull.Value),
+    };
+
+            dt = DBConnect.ExecuteQuery("SP_SELECT_COURSE_DETAIL", selectparams);
 
             if (dt?.Rows?.Count > 0)
             {
-                khoaHoc = dt;
-                return khoaHoc;
+                courseDetail = dt;
+                return courseDetail;
             }
-            return khoaHoc;
+
+            return courseDetail;
         }
-        [HttpPost]
-        [Route("themThongTinKhoaHoc")]
-        public bool ThemThongTinKhoaHoc([FromBody] KhoaHoc data)
-        {
-            bool result = false;
 
-            SqlParameter[] insertparams = {
-                    new SqlParameter("@MaKH", data.MaKH),
-                    new SqlParameter("@TenKH", data.TenKH),
-                    new SqlParameter("@HocPhi", data.HocPhi),
-                    new SqlParameter("@HeSoLyThuyet", data.HeSoLyThuyet),
-                    new SqlParameter("@HeSoThucHanh", data.HeSoThucHanh),
-                    new SqlParameter("@HeSoDuAn", data.HeSoDuAn),
-                    new SqlParameter("@HeSoCuoiKy", data.HeSoCuoiKy),
-            };
+        //API Course
+        /*   [HttpGet]
+           [Route("thongTinKhoaHoc")]
+           public object DanhSachKhoaHoc()
+           {
+               object khoaHoc = new List<object>();
+               DataTable dt = new DataTable();
 
-            result = DBConnect.ExecuteNonQuery("SP_INSERT_COURSE", insertparams);
+               dt = DBConnect.ExecuteQuery("SP_SELECT_SEARCH_COURSE");
 
-            return result;
-        }
-        [HttpPost]
-        [Route("suaThongTinKhoaHoc")]
-        public bool DoiThongTinKhoaHoc([FromBody] KhoaHoc data)
-        {
-            bool result = false;
+               if (dt?.Rows?.Count > 0)
+               {
+                   khoaHoc = dt;
+                   return khoaHoc;
+               }
+               return khoaHoc;
+           }
+           [HttpPost]
+           [Route("themThongTinKhoaHoc")]
+           public bool ThemThongTinKhoaHoc([FromBody] KhoaHoc data)
+           {
+               bool result = false;
 
-            SqlParameter[] updateparams = {
-                    new SqlParameter("@MaKH", data.MaKH),
-                    new SqlParameter("@TenKH", data.TenKH),
-                    new SqlParameter("@HocPhi", data.HocPhi),
-                    new SqlParameter("@HeSoLyThuyet", data.HeSoLyThuyet),
-                    new SqlParameter("@HeSoThucHanh", data.HeSoThucHanh),
-                    new SqlParameter("@HeSoDuAn", data.HeSoDuAn),
-                    new SqlParameter("@HeSoCuoiKy", data.HeSoCuoiKy),
-            };
+               SqlParameter[] insertparams = {
+                       new SqlParameter("@MaKH", data.MaKH),
+                       new SqlParameter("@TenKH", data.TenKH),
+                       new SqlParameter("@HocPhi", data.HocPhi),
+                       new SqlParameter("@HeSoLyThuyet", data.HeSoLyThuyet),
+                       new SqlParameter("@HeSoThucHanh", data.HeSoThucHanh),
+                       new SqlParameter("@HeSoDuAn", data.HeSoDuAn),
+                       new SqlParameter("@HeSoCuoiKy", data.HeSoCuoiKy),
+               };
 
-            result = DBConnect.ExecuteNonQuery("SP_UPDATE_COURSE", updateparams);
+               result = DBConnect.ExecuteNonQuery("SP_INSERT_COURSE", insertparams);
 
-            return result;
-        }
+               return result;
+           }
+           [HttpPost]
+           [Route("suaThongTinKhoaHoc")]
+           public bool DoiThongTinKhoaHoc([FromBody] KhoaHoc data)
+           {
+               bool result = false;
+
+               SqlParameter[] updateparams = {
+                       new SqlParameter("@MaKH", data.MaKH),
+                       new SqlParameter("@TenKH", data.TenKH),
+                       new SqlParameter("@HocPhi", data.HocPhi),
+                       new SqlParameter("@HeSoLyThuyet", data.HeSoLyThuyet),
+                       new SqlParameter("@HeSoThucHanh", data.HeSoThucHanh),
+                       new SqlParameter("@HeSoDuAn", data.HeSoDuAn),
+                       new SqlParameter("@HeSoCuoiKy", data.HeSoCuoiKy),
+               };
+
+               result = DBConnect.ExecuteNonQuery("SP_UPDATE_COURSE", updateparams);
+
+               return result;
+           }*/
 
         //[HttpPost]
         //[Route("xoaThongTinHocVien")]
