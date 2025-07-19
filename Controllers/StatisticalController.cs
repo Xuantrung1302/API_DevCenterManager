c2300962f9356b464dfa54cf3aa9a613a629c620
Huymess11 <huymess0610@gmail.com>
2025-07-19 14:17:26 +0700

    commit StatisticalController

---
+++ Controllers/StatisticalController.cs
@@ -0,0 +1,38 @@
+﻿using API_Technology_Students_Manages.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API_Technology_Students_Manages.Controllers
+{
+    [RoutePrefix("api/Statistical")]
+    public class StatisticalController : ApiController
+    {
+        DBConnect DBConnect = new DBConnect();
+
+        [HttpGet]
+        [Route("thongKeSoHocSinhDangKyHocTheoThangTrongNam")]
+        public object SoHocSinhDangKy(string year = null)
+        {
+            object subject = new List<object>();
+            DataTable dt = new DataTable();
+            SqlParameter[] selectParams = {
+                new SqlParameter("@Year", year)
+            };
+
+            dt = DBConnect.ExecuteQuery("SP_COUNT_ENROLLMENT_BY_MONTH_YEAR", selectParams);
+
+            if (dt?.Rows?.Count > 0)
+            {
+                subject = dt;
+                return subject;
+            }
+            return subject;
+        }
+    }
+}
